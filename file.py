0df9c5f527a7d6ce758a324de326bf29833983b7
lyndseymitsch <114434903+lyndseymitsch@users.noreply.github.com>
2022-11-15 11:31:40 -0800

    Update file.py

--- file.py
+++ file.py
@@ -3,3 +3,5 @@ from matplotlib import pyplot as plt
 from sklearn import tree, preprocessing
 import numpy as np
 
+url = "https://philchodrow.github.io/PIC16A/datasets/palmer_penguins.csv"
+penguins = pd.read_csv(url)
